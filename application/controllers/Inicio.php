d6d1770010d922793dc240f849299f90e02a9115
Miguel Angel Vázquez Vázquez <mvk_12@outlook.com>
2017-06-17 09:33:02 -0500

    Uso de Twig

--- application/controllers/Inicio.php
+++ application/controllers/Inicio.php
@@ -24,5 +24,25 @@ class Inicio extends CI_Controller {
         $this->load->view('inicio', $array);
     }
 
+    public function uso_twig()
+    {
+        $obj = new stdClass();
+        $obj->prop1 = 1;
+        $obj->prop2 = 'Lectura';
+
+        $array = array(
+            'numero' => 1,
+            'cadena' => 'Aliquam nam perspiciatis quibusdam adipisci eaque ipsum.',
+            'flotante' => 122.55,
+            'arr' => array(
+                'dato1' => 'Esto',
+                'dato2' => 'es',
+                'dato3' => 'una',
+                'dato4' => 'prueba',
+            ),
+            'objeto' => $obj,
+        );
+        $this->twig->display('uso_twig', $array);
+    }
 }
 /* End of file Inicio.php */
